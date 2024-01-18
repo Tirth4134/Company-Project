1b97297c62fdeda5d3f4cb44b289e5d26705b379
jay suthar <tirth4134@gmail.com>
2024-01-18 17:38:46 +0530

    #

--- next-jamstacky/components/Header.tsx
+++ next-jamstacky/components/Header.tsx
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-"use client";
-=======
  "use client";
->>>>>>> Stashed changes
 import client from "@/client/sanity.client";
 import { urlFor } from "@/imageBuilder/urlFor";
 import { HiOutlineMenu } from "react-icons/hi";
@@ -50,7 +46,6 @@ const Header = async () => {
             key={list?._id}
             className="max-w-[90rem] m-auto py-[1.9375rem] flex items-center justify-between relative"
           >
-<<<<<<< Updated upstream
             <div className="flex items-center gap-[0.56rem] justify-center">
               <Link href={"/"}>
                 {" "}
@@ -60,7 +55,6 @@ const Header = async () => {
                   width={219}
                   height={41}
                 />
-=======
             <div className="flex items-center gap-[0.56rem]">
             <Link href={"/"}>  
                 <img
@@ -69,7 +63,6 @@ const Header = async () => {
                 width={180}
                 height={180}
               />
->>>>>>> Stashed changes
               </Link>
               {/* <h3 className="text-[2.125rem] font-[500] leading-[2.65625rem] text-center text-blue max-w-[11.0625rem] font-dmSans">{data.logo.logo_text}</h3> */}
             </div>
@@ -91,37 +84,10 @@ const Header = async () => {
                 {list?.button?.label}
               </Link>
             </nav>
-<<<<<<< Updated upstream
             <HiOutlineMenu
               className="text-[2.375rem] md:hidden block text-buttoncolor"
               onClick={handleMenu}
             />
-=======
-            <HiOutlineMenu className="text-[2.375rem] md:hidden block text-buttoncolor" onClick={handleMenu} />
-
-{/* 
-                             { <div className=" bg-buttoncolor my-[1.5rem] w-[97%] rounded-[0.9375rem] md:hidden block  fixed top-[-4%] mb:top-[4%] z-[1] py-[1rem] px-[1rem] left-[51%] translate-x-[-50%] ">
-                                <div className="flex items-end justify-end">
-                                    <IoClose className="text-[2.375rem] m:hidden block text-whitecolor absolute top-[-3%] mb:top-[4%] " onClick={handleMenu} />
-                                </div>
-                                <nav className="flex flex-col gap-[1rem]">
-                                    {
-                                        list?.nav_links.map((item: any,inedx:number) => {
-
-                                            return (
-                                                <li key={inedx} className="text-[1.375rem] list-none font-[400] text-whitecolor w-[7.6875rem] font-dmSans">
-                                                    <Link href={item.slug.current}>{list.label}</Link>
-                                                </li>
-                                            )
-                                        })
-                                    }
-                                    <button className="flex items-center w-fit justify-center px-[3.21rem] py-[1.25rem] rounded-[3.5rem] bg-blue text-[1.125rem] text-center font-[700] leading-[1.25rem] text-whitecolor font-dmSans ">
-                                        {list.button}
-                                    </button>
-                                </nav>
-                            </div>
-                            }  */}
->>>>>>> Stashed changes
           </div>
         );
       })}
