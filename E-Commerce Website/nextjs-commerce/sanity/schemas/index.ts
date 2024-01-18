e1b949bac1b9b1e939a6df510fa7ea48119e905e
Tirth Patel <tirth4134@gmail.com>
2024-01-18 10:19:21 +0530

    #

--- E-Commerce Website/nextjs-commerce/sanity/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import categeory from "./categeory";
-import heroImages from "./heroImages";
-import product from "./product";
-
-export const schemaTypes = [product, categeory, heroImages]
--- next-jamstacky/app/page.tsx
+++ next-jamstacky/app/page.tsx
@@ -18,7 +18,7 @@ export default async function page() {
         
         return(   
             <>
-            
+                <Blog_home />
              <Homehero />
             <CaseStudy />
             <Hometechnology />
